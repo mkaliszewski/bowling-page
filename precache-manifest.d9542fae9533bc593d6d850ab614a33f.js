f883777ff630e8752f9f12d62a1a06ab5d942585
mikibaka <kaliszewski.mikolaj@gmail.com>
2020-10-21 20:45:10 +0200

    Updates

---
+++ precache-manifest.d9542fae9533bc593d6d850ab614a33f.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b24bf1fdacdfe70175575b99e8720c",
+    "url": "/bowling-page/index.html"
+  },
+  {
+    "revision": "0e50c70d26c0f1089882",
+    "url": "/bowling-page/static/css/main.df806b13.chunk.css"
+  },
+  {
+    "revision": "788e661d243bfc2b428d",
+    "url": "/bowling-page/static/js/2.f3a08b19.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/bowling-page/static/js/2.f3a08b19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e50c70d26c0f1089882",
+    "url": "/bowling-page/static/js/main.2cb83967.chunk.js"
+  },
+  {
+    "revision": "8c3ea924b8e802b1868e",
+    "url": "/bowling-page/static/js/runtime-main.f513686d.js"
+  },
+  {
+    "revision": "d7b6263d99b1ef2dc34ed6781af17f47",
+    "url": "/bowling-page/static/media/arrow.d7b6263d.svg"
+  },
+  {
+    "revision": "e971ae77adcbe5b3c6e180fef0c94b13",
+    "url": "/bowling-page/static/media/billard.e971ae77.svg"
+  },
+  {
+    "revision": "4716bf9568d0f7e0089b84a0a73716dc",
+    "url": "/bowling-page/static/media/bowling.4716bf95.svg"
+  },
+  {
+    "revision": "ce4d1e64e82576231cf075050e6269ba",
+    "url": "/bowling-page/static/media/drink_2.ce4d1e64.png"
+  },
+  {
+    "revision": "a51cb7687de89cd78db809bec13bcdad",
+    "url": "/bowling-page/static/media/facebook.a51cb768.svg"
+  },
+  {
+    "revision": "d0da3183551dc720833c106d7ab65d29",
+    "url": "/bowling-page/static/media/galleryImage1.d0da3183.png"
+  },
+  {
+    "revision": "8e32bd5cf46642192553f8f896ea7ba0",
+    "url": "/bowling-page/static/media/galleryImage2.8e32bd5c.png"
+  },
+  {
+    "revision": "406c1765c963622dc06d4feaa9623680",
+    "url": "/bowling-page/static/media/galleryImage3.406c1765.png"
+  },
+  {
+    "revision": "468b82d915d1ef633d126bbd757826c9",
+    "url": "/bowling-page/static/media/galleryImage4.468b82d9.png"
+  },
+  {
+    "revision": "881dfef1e0b44cd67f13c9cfad368b1d",
+    "url": "/bowling-page/static/media/galleryImage5.881dfef1.jpg"
+  },
+  {
+    "revision": "4452dc444e810fdbd417a239a0b49916",
+    "url": "/bowling-page/static/media/galleryImage6.4452dc44.png"
+  },
+  {
+    "revision": "bee4bf5d491e1bb1b20ef07d0d9f03c8",
+    "url": "/bowling-page/static/media/google_maps.bee4bf5d.svg"
+  },
+  {
+    "revision": "15d86713ecb3d510e6c823896257fa74",
+    "url": "/bowling-page/static/media/header.15d86713.jpg"
+  },
+  {
+    "revision": "fa94d44bad09afd9fbfca17edeca494c",
+    "url": "/bowling-page/static/media/mail.fa94d44b.svg"
+  },
+  {
+    "revision": "4452dc444e810fdbd417a239a0b49916",
+    "url": "/bowling-page/static/media/paralax_1.4452dc44.png"
+  },
+  {
+    "revision": "229112393b8c6d8e6b8ef5e2523fdb77",
+    "url": "/bowling-page/static/media/paralax_2.22911239.jpg"
+  },
+  {
+    "revision": "881dfef1e0b44cd67f13c9cfad368b1d",
+    "url": "/bowling-page/static/media/paralax_3.881dfef1.jpg"
+  },
+  {
+    "revision": "ff09e6f9dc1a1afe34c5a7fe43ffa34a",
+    "url": "/bowling-page/static/media/phone.ff09e6f9.svg"
+  },
+  {
+    "revision": "04718cb8818b6073d022915059cd6c7f",
+    "url": "/bowling-page/static/media/phone_footer.04718cb8.svg"
+  },
+  {
+    "revision": "e6f59738b04cba45cc5b4ec2bad7bf56",
+    "url": "/bowling-page/static/media/price.e6f59738.svg"
+  }
+]);
